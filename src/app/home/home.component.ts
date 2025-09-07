3fb4d05aa004641b844ace0cca8f24760cd046b7
Esther Manrique <esthermanriqueepam@gmail.com>
2025-09-07 08:11:29 -0600

    CRUD implemented - Service Layer

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,66 @@
-import {Component, computed, effect, inject, Injector, signal} from '@angular/core';
-import {CoursesService} from "../services/courses.service";
-import {Course, sortCoursesBySeqNo} from "../models/course.model";
-import {MatTab, MatTabGroup} from "@angular/material/tabs";
-import {CoursesCardListComponent} from "../courses-card-list/courses-card-list.component";
-import {MatDialog} from "@angular/material/dialog";
-import {MessagesService} from "../messages/messages.service";
-import {catchError, from, throwError} from "rxjs";
-import {toObservable, toSignal, outputToObservable, outputFromObservable} from "@angular/core/rxjs-interop";
+import {
+  Component,
+  computed,
+  effect,
+  inject,
+  Injector,
+  signal,
+} from '@angular/core';
+import { CoursesService } from '../services/courses.service';
+import { Course, sortCoursesBySeqNo } from '../models/course.model';
+import { MatTab, MatTabGroup } from '@angular/material/tabs';
+import { CoursesCardListComponent } from '../courses-card-list/courses-card-list.component';
+import { MatDialog } from '@angular/material/dialog';
+import { MessagesService } from '../messages/messages.service';
+import { catchError, from, throwError } from 'rxjs';
+import {
+  toObservable,
+  toSignal,
+  outputToObservable,
+  outputFromObservable,
+} from '@angular/core/rxjs-interop';
+import { CoursesServiceWithFetch } from '../services/courses-fetch.service';
 
 @Component({
-    selector: 'home',
-    imports: [
-        MatTabGroup,
-        MatTab,
-        CoursesCardListComponent
-    ],
-    templateUrl: './home.component.html',
-    styleUrl: './home.component.scss'
+  selector: 'home',
+  imports: [MatTabGroup, MatTab, CoursesCardListComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  #courses = signal<Course[]>([]);
 
+  coursesService = inject(CoursesService);
+
+  beginnerCourses = computed(() => {
+    const courses = this.#courses();
+    return courses.filter((course) => course.category === 'BEGINNER');
+  });
+
+  advancedCourses = computed(() => {
+    const courses = this.#courses();
+    return courses.filter((course) => course.category === 'ADVANCED');
+  });
+
+  constructor() {
+
+    effect(() => {
+        console.log(`Beginner courses:`, this.beginnerCourses());
+        console.log(`Advanced courses:`, this.advancedCourses());
+    });
+
+    this.loadCourses().then(() =>
+      console.log(`All Courses loaded:`, this.#courses())
+    );
+  }
+
+  async loadCourses() {
+    try {
+      const courses = await this.coursesService.loadAllCourses();
+      this.#courses.set(courses.sort(sortCoursesBySeqNo));
+    } catch (err) {
+      alert('Error loading courses!');
+      console.log('Error loading courses', err);
+    }
+  }
 }
