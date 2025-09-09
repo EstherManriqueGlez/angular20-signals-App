d5836242e285641546bf4ae1a9b8d0a003e623f8
Esther Manrique <esthermanriqueepam@gmail.com>
2025-09-08 19:14:18 -0600

    Add course and lessons components with resolvers and update routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
-import {Routes} from '@angular/router';
-import {HomeComponent} from "./home/home.component";
-import {LoginComponent} from "./login/login.component";
-import {LessonsComponent} from "./lessons/lessons.component";
-import {ResourceDemoComponent} from "./resource-demo/resource-demo.component";
-import {LinkedSignalDemoComponent} from "./linked-signal/linked-signal-demo.component";
-import { isUserAuthenticated } from './guards/auth.guard';
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { LessonsComponent } from './lessons/lessons.component';
+import { ResourceDemoComponent } from './resource-demo/resource-demo.component';
+import { LinkedSignalDemoComponent } from './linked-signal/linked-signal-demo.component';
+import { CourseComponent } from './course/course.component';
+import { courseResolver } from './course/course.resolver';
+import { courseLessonsResolver } from './course/course-lessons.resolver';
+// import { isUserAuthenticated } from './guards/auth.guard';
 
 export const routes: Routes = [
   {
@@ -13,23 +16,32 @@ export const routes: Routes = [
     // canActivate: [isUserAuthenticated]
   },
   {
-    path: "login",
-    component: LoginComponent
+    path: 'courses/:courseId',
+    component: CourseComponent,
+    // canActivate: [isAuthenticated],
+    resolve: {
+      course: courseResolver,
+      lessons: courseLessonsResolver
+    },
   },
   {
-    path: "lessons",
-    component: LessonsComponent
+    path: 'login',
+    component: LoginComponent,
   },
   {
-    path:"shopping-cart",
-    component: LinkedSignalDemoComponent
+    path: 'lessons',
+    component: LessonsComponent,
   },
   {
-    path: "resource-demo",
-    component: ResourceDemoComponent
+    path: 'shopping-cart',
+    component: LinkedSignalDemoComponent,
+  },
+  {
+    path: 'resource-demo',
+    component: ResourceDemoComponent,
   },
   {
     path: '**',
-    redirectTo: '/'
-  }
+    redirectTo: '/',
+  },
 ];
